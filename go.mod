7c9f9081664acbbcf1e0901be55c59c0426169c8
moyiz <8603313+moyiz@users.noreply.github.com>
2023-12-13 20:20:18 +0200

    tests: add tests for completions and config related actions

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21
 require (
 	github.com/adrg/xdg v0.4.0
 	github.com/mitchellh/go-ps v1.0.0
+	github.com/spf13/afero v1.11.0
 	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.17.0
 )
@@ -19,7 +20,6 @@ require (
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
---
+++ internal/cli/completion_test.go
@@ -0,0 +1,66 @@
+package cli
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/moyiz/na/internal/config"
+)
+
+type ListNextPartsTestCase struct {
+	Aliases  []config.Alias
+	Prefix   []string
+	Expected []string
+}
+
+func TestListNextParts(t *testing.T) {
+	var result []string
+
+	aliases := []config.Alias{
+		{
+			Name:    "my command",
+			Command: "cmd1",
+		},
+		{
+			Name:    "my other command",
+			Command: "cmd1",
+		},
+		{
+			Name:    "another command2",
+			Command: "cmd1",
+		},
+	}
+	cases := []ListNextPartsTestCase{
+		{
+			Aliases:  aliases,
+			Prefix:   []string{},
+			Expected: []string{"my", "another"},
+		},
+		{
+			Aliases:  aliases,
+			Prefix:   []string{"my"},
+			Expected: []string{"command", "other"},
+		},
+		{
+			Aliases:  aliases,
+			Prefix:   []string{"my", "other"},
+			Expected: []string{"command"},
+		},
+		{
+			Aliases:  aliases,
+			Prefix:   []string{"another", "command2"},
+			Expected: []string{},
+		},
+		{
+			Aliases:  aliases,
+			Prefix:   []string{"non", "existing"},
+			Expected: []string{},
+		},
+	}
+	for _, testCase := range cases {
+		result = ListNextParts(aliases, testCase.Prefix)
+		if slices.Compare(result, testCase.Expected) != 0 {
+			t.Errorf("Expected %v but got %v", testCase.Expected, result)
+		}
+	}
+}
